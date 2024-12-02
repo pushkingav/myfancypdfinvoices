fa78b352674ebad4d17fc3f17afa3dbf332bc169
Andrei Pushkin <av.pushking@gmail.com>
2024-12-02 11:49:59 +0400

    Add @ComponentScan and @Components (singletons)

--- src/main/java/com/apushkin/context/MyFancyPdfInvoicesApplicationConfiguration.java
+++ src/main/java/com/apushkin/context/MyFancyPdfInvoicesApplicationConfiguration.java
@@ -1,20 +1,21 @@
 package com.apushkin.context;
 
-import com.apushkin.service.InvoiceService;
-import com.apushkin.service.UserService;
+import com.apushkin.ApplicationLauncher;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.javafaker.Faker;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackageClasses = ApplicationLauncher.class)
 public class MyFancyPdfInvoicesApplicationConfiguration {
     @Bean
     public Faker faker() {
         return new Faker();
     }
 
-    @Bean
+    /*@Bean
     public UserService userService(Faker faker) {
         return new UserService(faker);
     }
@@ -22,7 +23,7 @@ public class MyFancyPdfInvoicesApplicationConfiguration {
     @Bean
     public InvoiceService invoiceService(UserService userService) {
         return new InvoiceService(userService);
-    }
+    }*/
 
     @Bean
     public ObjectMapper objectMapper() {
